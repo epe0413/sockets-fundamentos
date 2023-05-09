aced935e5f1039099760e24569efa56dbce564c1
Everth Paredes <epe0413@gmail.com>
2023-05-09 13:59:46 -0600

    Mensajes de conexión y desconexión - Cliente

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,19 @@
+// Referencia HTML
+const lblOnline = document.querySelector('#lblOnline')
+const lblOffline = document.querySelector('#lblOffline')
+
 const socket = io();
+
+socket.on('connect', () => {
+    console.log('conectado');
+
+    lblOffline.style.display = 'none'
+    lblOnline.style.display = ''
+});
+
+socket.on('disconnect', () => {
+    console.log('Desconectado del servidor');
+
+    lblOnline.style.display = 'none'
+    lblOffline.style.display = ''
+});
